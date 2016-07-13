b815a4ce05ab8c375109bf6f4f09f5ce709650d0
kimihiro64 <jonaswhidden@gmail.com>
2016-07-13 11:48:23 -0500

    Update bitcoin to lbrycrd

--- gui/stdio.py
+++ gui/stdio.py
@@ -3,7 +3,7 @@ _ = lambda x:x
 #from i18n import _
 from lbryum.wallet import WalletStorage, Wallet
 from lbryum.util import format_satoshis, set_verbosity, StoreDict
-from lbryum.bitcoin import is_valid, COIN, TYPE_ADDRESS
+from lbryum.lbrycrd import is_valid, COIN, TYPE_ADDRESS
 from lbryum.network import filter_protocol
 import sys, getpass, datetime
 
