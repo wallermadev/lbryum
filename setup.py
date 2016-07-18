b490d4a25c8411cfb1cf42dddfcd0b072aa5e671
Job Evers <jobevers@users.noreply.github.com>
2016-07-18 15:58:53 -0500

    upgrade to protobuf-3.0.0b3

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         'pbkdf2',
         'requests',
         'qrcode',
-        'protobuf<=3.0.0b2',
+        'protobuf==3.0.0b3',
         'dnspython',
         'jsonrpclib',
         'six==1.9.0',
